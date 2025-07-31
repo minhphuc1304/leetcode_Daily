8009c74f3252fd3cc6749e63e262baa43dd6bb04
Minh Phúc <73222241+minhphuc1304@users.noreply.github.com>
2025-07-31 09:56:51 +0700

    Create subarrayBitwiseORs.js

---
+++ 202507July/310725_subarrayBitwiseORs/subarrayBitwiseORs.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var subarrayBitwiseORs = function(arr) {
+    let res = new Set();
+    let cur = new Set();
+    for (let num of arr) {
+        let next = new Set([num]);
+        for (let x of cur) {
+            next.add(x | num);
+        }
+        cur = next;
+        for (let x of cur) res.add(x);
+    }
+    return res.size;
+};
