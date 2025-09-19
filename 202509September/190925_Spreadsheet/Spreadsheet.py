273c10e07f4fbea4008c96023d765279a96e5d33
Minh Phúc <73222241+minhphuc1304@users.noreply.github.com>
2025-09-19 16:42:00 +0700

    Update Spreadsheet.py

--- 202509September/190925_Spreadsheet/Spreadsheet.py
+++ 202509September/190925_Spreadsheet/Spreadsheet.py
@@ -1,13 +1,16 @@
 class Spreadsheet:
 
     def __init__(self, rows: int):
+        
         self.map = dict()        
 
-    def setCell(self, cell: str, value: int) -> None:        
+    def setCell(self, cell: str, value: int) -> None:    
+        
         self.map[cell] = value
 
     def resetCell(self, cell: str) -> None:
-        if(cell in self.map):
+        
+        if (cell in self.map):
             self.map.pop(cell)          
 
     def getValue(self, formula: str) -> int:
@@ -17,18 +20,18 @@ class Spreadsheet:
         t1 = formula[0]
         t2 = formula[1]
 
-        if(t1.isdigit()):
+        if (t1.isdigit()):
             t1 = int(t1)    
         else:
-            if(t1 not in self.map):
+            if (t1 not in self.map):
                 t1 = 0
             else:
                 t1 = self.map[t1]
         
-        if(t2.isdigit()):
+        if (t2.isdigit()):
             t2 = int(t2)    
         else:
-            if(t2 not in self.map):
+            if (t2 not in self.map):
                 t2 = 0
             else:
                 t2 = self.map[t2]
