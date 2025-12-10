4dd702361182c1e3938fac0de3f19e25fba843a3
Minh Phúc <73222241+minhphuc1304@users.noreply.github.com>
2025-12-10 10:33:24 +0700

    Create countPermutations.cpp

---
+++ 202512December/101225_countPermutations/countPermutations.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countPermutations(vector<int>& complexity) {
+        
+        int n = complexity.size();
+        long long int ans = 1;
+        const int mod = 1e9 + 7;
+
+        for (int i = 1; i < n; i++) {
+            if(complexity[i] <= complexity[0]) {
+                return 0;
+            }
+            ans = (ans*1LL*(i))%mod;
+        }
+        return ans;
+    }
+};
