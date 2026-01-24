b077dd541395d5783b4a30c5c015803c09a5bed8
Minh Phúc <73222241+minhphuc1304@users.noreply.github.com>
2026-01-24 10:16:18 +0700

    Create minPairSum.py

---
+++ 202601January/240126_minPairSum/minPairSum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minPairSum(self, nums: List[int]) -> int:
+        nums.sort()
+
+        left = 0
+        right = len(nums) - 1
+        max_pair_sum = 0
+
+        while left < right:
+            current_sum = nums[left] + nums[right]
+            if current_sum > max_pair_sum:
+                max_pair_sum = current_sum
+
+            left += 1
+            right -= 1
+
+        return max_pair_sum
