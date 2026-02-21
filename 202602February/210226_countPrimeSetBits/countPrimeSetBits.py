0ceed4a511d935a8eae38d88df98a54169c28ff5
Minh Phúc <73222241+minhphuc1304@users.noreply.github.com>
2026-02-21 09:55:24 +0700

    Create countPrimeSetBits.py

---
+++ 202602February/210226_countPrimeSetBits/countPrimeSetBits.py
@@ -0,0 +1,26 @@
+class Solution:
+  def countPrimeSetBits(self, left: int, right: int) -> int:
+
+    def prime(num):
+      if num <= 1:
+        return False
+      if num <= 3:
+        return True
+      if num % 2 == 0 or num % 3 == 0:
+        return False
+      
+      i = 5
+      while i*i <= num:
+        if num%i == 0 or num%(1+2) == 0:
+          return False
+      
+      return True
+    
+    cnt = 0
+    for i in range(left, right + 1):
+      val = bin(i)[2:]
+      res = val.count('1')
+      if prime(res) == True:
+        cnt += 1
+    
+    return cnt
