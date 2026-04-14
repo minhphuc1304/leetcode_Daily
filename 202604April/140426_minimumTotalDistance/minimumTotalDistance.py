80a777a7087668452f8d7cddec388bbe112d28fc
Minh Phúc <73222241+minhphuc1304@users.noreply.github.com>
2026-04-14 09:42:16 +0700

    Implement minimumTotalDistance function in Solution class

---
+++ 202604April/140426_minimumTotalDistance/minimumTotalDistance.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minimumTotalDistance(self, robots: List[int], factories: List[List[int]]) -> int:
+        robots.sort()
+        factories.sort()
+
+        factory_positions = []
+        for factory in factories:
+            for i in range(factory[1]):
+                factory_positions.append(factory[0])
+
+        robot_count = len(robots)
+        factory_count = len(factory_positions)
+        next_dist = [0 for _ in range(factory_count + 1)]
+        current = [0 for _ in range(factory_count + 1)]
+
+        current[factory_count] = 1e12
+
+        for i in range(robot_count - 1, -1, -1):
+            for j in range(factory_count - 1, -1 , -1):
+                assign = (
+                    abs(robots[i] -factory_positions[j]) + next_dist[j + 1]
+                )
+
+                skip = current[j + 1]
+                current[j] = min(assign, skip)
+
+            next_dist = current[:]
+
+        return current[0]
